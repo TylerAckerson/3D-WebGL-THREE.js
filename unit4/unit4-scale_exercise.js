587960600a1a0ed84215b620168e2daa4784d928
Tyler Ackerson <tackersonMSU12@gmail.com>
2015-12-20 17:57:10 -0800

    Completes scaling exercise

--- unit4/unit4-scale_exercise.js
+++ unit4/unit4-scale_exercise.js
@@ -80,7 +80,8 @@ function fillScene() {
 	sphere.position.y = 18;	// move the hand above the other hand
 
 	// YOUR CODE HERE:
-
+	sphere.scale = new THREE.Vector3(3, 0.2, 0.2);
+	sphere.rotation.y = 30 * Math.PI/180;
 	scene.add( sphere );
 }
 
